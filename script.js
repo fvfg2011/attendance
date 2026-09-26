f084bf880bcb35bce77fa465f508d265a6eda77f
fvfg2011 <fvfg20114322@gmail.com>
2026-09-27 01:09:26 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let currentDate = new Date();
 
 function formatDate(date) {
-  return date.toISOString().split("T")[0];
+  const y = date.getFullYear();
+  const m = String(date.getMonth() + 1).padStart(2, "0");
+  const d = String(date.getDate()).padStart(2, "0");
+  return `${y}-${m}-${d}`;
 }
-
 function formatReadable(date) {
   return date.toLocaleDateString("uk-UA", { day: "numeric", month: "long", year: "numeric" });
 }
